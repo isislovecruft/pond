4c86b920d4695591055cee61c40da28a0b0f9370
Isis Lovecruft <isis@torproject.org>
2013-12-30 03:13:04 +0000

    Remove TPM support.

--- client/erasure_gui_linux.go
+++ client/erasure_gui_linux.go
@@ -3,7 +3,6 @@
 package main
 
 import (
-	"fmt"
 	"time"
 
 	"github.com/agl/pond/client/disk"
@@ -63,16 +62,6 @@ func (c *guiClient) createErasureStorage(pw string, stateFile *disk.StateFile) e
 
 	var logText string
 
-	tpm := disk.TPM{
-		Log: func(format string, args ...interface{}) {
-			c.log.Printf(format, args...)
-			logText += fmt.Sprintf(format, args...) + "\n"
-			c.gui.Actions() <- SetTextView{name: "log", text: logText}
-			c.gui.Signal()
-		},
-		Rand: c.rand,
-	}
-
 NextEvent:
 	for {
 		event, ok := <-c.gui.Events()
@@ -90,6 +79,7 @@ NextEvent:
 			stateFile.Erasure = nil
 			return stateFile.Create(pw)
 		case "tpm":
+			panic("tpm support has been disabled")
 			if len(logText) > 0 {
 				c.gui.Actions() <- SetTextView{name: "log", text: ""}
 				c.gui.Signal()
@@ -97,13 +87,10 @@ NextEvent:
 				time.Sleep(300 * time.Millisecond)
 			}
 
-			stateFile.Erasure = &tpm
 			c.gui.Actions() <- Sensitive{name: "tpm", sensitive: false}
 			c.gui.Actions() <- Sensitive{name: "continue", sensitive: false}
 			c.gui.Signal()
 			if err := stateFile.Create(pw); err != nil {
-				tpm.Log("Setup failed with error: %s", err)
-				tpm.Log("You can click the button to try again")
 				c.gui.Actions() <- Sensitive{name: "tpm", sensitive: true}
 				c.gui.Actions() <- Sensitive{name: "continue", sensitive: true}
 				c.gui.Signal()
